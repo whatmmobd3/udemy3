710863b7402d33ab61ff41c31c380b835b670c8d
DAVID <david@DAVIDs-MacBook-Pro.local>
2020-02-17 06:55:16 +0700

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import PostList from './PostList'
 
 const App = () => {
     return(
-        <div>Blog</div>
+        <div className="ui container"><PostList /></div>
     )
 }
 
